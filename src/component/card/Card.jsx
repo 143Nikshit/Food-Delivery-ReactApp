7b70e2bbf1ca04e925e16cc986c95b69fc23efea
Nikshit <kingjoshi27032@gmail.com>
2026-06-22 20:47:30 +0530

    Adding CSS on Home Page

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -5,17 +5,19 @@ const Card = ({ cardData }) => {
     console.log(cardData)
     return (
         <div className='card-container'>
-            {cardData.map((item) => {
-                return (
-                    <div className='card' key={item.id}>
-                        <h1>{item.name}</h1>
-                        <img src={item.img} alt="" />
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-                        <p className='price'>price: &#8377;{item.price}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                )
-            })}
+            {cardData.length === 0 ? (<h2>No item found</h2>) : (
+                    cardData.map((item) => {
+                        return (
+                            <div className='card' key={item.id}>
+                                <h1>{item.name}</h1>
+                                <img src={item.img} alt="" />
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+                                <p className='price'>price: &#8377;{item.price}</p>
+                                <button>Add to Cart</button>
+                            </div>
+                        )
+                    })
+                )}
         </div>
     )
 }
